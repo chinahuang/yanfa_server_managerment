2321dc7d973dd4767363bd30e19e6de2c8c080ac
chinahuang <1543166885@qq.com>
2018-04-15 10:37:52 +0800

    init asset.model

--- SerMag/assert/models.py
+++ SerMag/assert/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class asset(models.Model):
+    hostname = models.CharField(max_length=48,null=True,blank=True,verbose_name='主机名')
+    asset_ID = models.CharField(max_length=20,null=True,blank=True,verbose_name='资产号')
+    server_model = models.CharField(max_length=20,null=True,blank=True)
+    inner_ip = models.GenericIPAddressField(verbose_name='内网IP',unique=True)
+    manage_ip = models.GenericIPAddressField(verbose_name='管理IP',unique=True)
