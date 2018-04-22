deff35a9484e9a6630c45d16fdcecd1e8b72c8ae
chinahuang <1543166885@qq.com>
2018-04-22 19:24:31 +0800

    edit model

--- SerMag/SerMag/settings.py
+++ SerMag/SerMag/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'assert',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'SerMag.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,13 +74,23 @@ WSGI_APPLICATION = 'SerMag.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SerMag',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- SerMag/assert/models.py
+++ SerMag/assert/models.py
@@ -7,6 +7,26 @@ from django.db import models
 class asset(models.Model):
     hostname = models.CharField(max_length=48,null=True,blank=True,verbose_name='主机名')
     asset_ID = models.CharField(max_length=20,null=True,blank=True,verbose_name='资产号')
-    server_model = models.CharField(max_length=20,null=True,blank=True)
+    server_model = models.CharField(max_length=20,null=True,blank=True,verbose_name='型号')
+    server_system = models.CharField(max_length=20,null=True,blank=True,verbose_name='系统')
+    server_ID = models.CharField(max_length=20,null=True,blank=True,verbose_name='序列号')
     inner_ip = models.GenericIPAddressField(verbose_name='内网IP',unique=True)
     manage_ip = models.GenericIPAddressField(verbose_name='管理IP',unique=True)
+    server_admin = models.CharField(max_length=20,null=True,blank=True,verbose_name='管理用户')
+    server_admin_passwd = models.CharField(max_length=20,null=True,blank=True,verbose_name='管理用户密码')
+    server_place = models.CharField(max_length=20,null=True,blank=True,verbose_name='资产地点')
+    server_cabinet = models.CharField(max_length=20,null=True,blank=True,verbose_name='机柜信息')
+    is_active = models.BooleanField(default=True,verbose_name='是否启用')
+    is_check = models.BooleanField(default=False,verbose_name='是否检查')
+    create_time = models.DateTimeField(auto_now_add=True,null=True,blank=True,verbose_name='创建时间')
+    update_time = models.DateTimeField(auto_now=True,null=True,blank=True,verbose_name='更新时间')
+
+    class Meta:
+        db_table = 'asset'
+        verbose_name = '资产管理'
+        verbose_name_plural = '资产管理'
+
+        def __str__(self):
+            return self.asset_ID
+
+
