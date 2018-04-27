021a1a2efbf8dd988c07366534fb54fb5d3e434c
chinahuang <1543166885@qq.com>
2018-04-27 23:53:51 +0800

    change asset model

--- SerMag/assert/models.py
+++ SerMag/assert/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import Group
 
 # Create your models here.
 class asset(models.Model):
@@ -12,12 +13,19 @@ class asset(models.Model):
     server_ID = models.CharField(max_length=20,null=True,blank=True,verbose_name='序列号')
     inner_ip = models.GenericIPAddressField(verbose_name='内网IP',unique=True)
     manage_ip = models.GenericIPAddressField(verbose_name='管理IP',unique=True)
-    server_admin = models.CharField(max_length=20,null=True,blank=True,verbose_name='管理用户')
-    server_admin_passwd = models.CharField(max_length=20,null=True,blank=True,verbose_name='管理用户密码')
+    manage_url = models.CharField(max_length=30,null=True,blank=True,verbose_name='管理url')
+    manage_user = models.CharField(max_length=15,null=True,blank=True,verbose_name='管理用户')
+    manage_user_passwd = models.CharField(max_length=20,null=True,blank=True,verbose_name='管理用户密码')
+    server_admin = models.CharField(max_length=20,null=True,blank=True,verbose_name='登陆用户')
+    server_admin_passwd = models.CharField(max_length=20,null=True,blank=True,verbose_name='登陆用户密码')
     server_place = models.CharField(max_length=20,null=True,blank=True,verbose_name='资产地点')
     server_cabinet = models.CharField(max_length=20,null=True,blank=True,verbose_name='机柜信息')
     is_active = models.BooleanField(default=True,verbose_name='是否启用')
     is_check = models.BooleanField(default=False,verbose_name='是否检查')
+    is_down = models.BooleanField(default=False,verbose_name='是否下架')
+    server_info = models.TextField(max_length=200,null=True,blank=True,verbose_name='服务器配置信息')
+    func = models.TextField(max_length=200,null=True,blank=True,verbose_name='功能')
+    desc = models.TextField(max_length=500,null=True,blank=True,verbose_name='描述')
     create_time = models.DateTimeField(auto_now_add=True,null=True,blank=True,verbose_name='创建时间')
     update_time = models.DateTimeField(auto_now=True,null=True,blank=True,verbose_name='更新时间')
 
